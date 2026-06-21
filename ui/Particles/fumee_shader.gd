56fb69801141b62c63a48cae944c0f9affa0bd06
destinix8 <destinix8@gmail.com>
2026-06-21 23:10:13 +0200

    shaders

--- ui/Particles/fumee_shader.gd
+++ ui/Particles/fumee_shader.gd
@@ -4,3 +4,4 @@ func _ready() -> void:
 	var vp = get_viewport_rect()
 	position = Vector2.ZERO
 	size = vp.size
+	mouse_filter = MOUSE_FILTER_IGNORE
---
+++ ui/Particles/rayons_lumiere.gd
@@ -0,0 +1,7 @@
+extends ColorRect
+
+func _ready() -> void:
+	var vp = get_viewport_rect()
+	position = Vector2.ZERO
+	size = vp.size
+	mouse_filter = MOUSE_FILTER_IGNORE
