6d50b913a28a35083c3e652d842c07bdfb56de9f
destinix8 <destinix8@gmail.com>
2026-06-23 20:59:20 +0200

    rien

--- ui/Particles/fumee_shader.gd
+++ ui/Particles/fumee_shader.gd
@@ -1,7 +1,19 @@
 extends ColorRect
 
+@export var densite: float = 0.5
+@export var vitesse: float = 0.048
+@export var echelle: float = 3.75
+@export var durete: float = 2.0
+@export var couleur: Color = Color(1, 1, 1, 1)
+
+
 func _ready() -> void:
 	var vp = get_viewport_rect()
 	position = Vector2.ZERO
 	size = vp.size
 	mouse_filter = MOUSE_FILTER_IGNORE
+	material.set_shader_parameter("densite", densite)
+	material.set_shader_parameter("vitesse", vitesse)
+	material.set_shader_parameter("echelle", echelle)
+	material.set_shader_parameter("durete", durete)
+	material.set_shader_parameter("couleur_fumee", couleur)
